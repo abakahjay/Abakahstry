40d855d55ca18e1308c5ce5d21f71f8b810d693c
abakahjay <joshuaabakahhh@gmail.com>
2024-10-04 01:10:22 +0000

    my google code

---
+++ index.js
@@ -0,0 +1,42 @@
+console.log("Hello World"); // console.log
+const name = "Abakah";      //name
+names ="Joshua Abakah";      //names
+console.log(name);          //testing const
+let d = typeof name;     // type
+console.log(d)
+let firstName = 'Joshua';
+let lastName = 'Abakah';
+let isApproved = true; //boolean
+console.log(isApproved);
+let selectedColor = null; //used to clear the value of the selected color
+console.log(selectedColor);
+let e = typeof selectedColor; //check the type of the selected color
+console.log(e)
+let person = {
+    namew: "Abakah",
+    age: 21
+};
+console.log(person);
+console.log(person.namew)
+person["namew"] = "Victor"; //Bracket Notation for Objects
+console.log(person["namew"]);
+person.namew = "Blessed";  //Dot Notation for Objects
+console.log(person.namew);
+let selectedColors = ["Red","yellow","Green","Blue","Violet","Indigo","Grey"]; //testing for list of colors
+console.log(selectedColors);
+console.log(selectedColors[0]);//1st Color on the List of Colors
+console.log(selectedColors[1]);//2nd Color on the List of Colors
+console.log(selectedColors[2]);//3rd Color on the List of Colors
+console.log(selectedColors[3]);//4th Color on the List of Colors
+console.log(selectedColors[4]);//5th Color on the List of Colors
+console.log(selectedColors[5]);//6th Color on the List of Colors
+console.log(selectedColors[6]);//7th Color on the List of Colors
+selectedColors[7] = "Black";// Adding Another color to the List of Colors
+console.log(selectedColors[7]);
+console.warn("Stopping");
+console.error("This is an Error");
+console.assert(false, "This is a successful assertion");
+console.log(selectedColors.length);
+
+
+
